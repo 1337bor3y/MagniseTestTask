95d23c1b069e7b903a782ffe28ea00c6233de545
1337bor3y <boreichuck.maxim@lll.kpi.ua>
2024-10-28 09:03:51 +0200

    Remove redundant API requsts

--- app/src/main/java/com/example/magnisetesttask/data/remote/retrofit/FintachartsApi.kt
+++ app/src/main/java/com/example/magnisetesttask/data/remote/retrofit/FintachartsApi.kt
@@ -1,18 +1,8 @@
 package com.example.magnisetesttask.data.remote.retrofit
 
-import com.example.magnisetesttask.data.remote.retrofit.dto.CountBackResponse
 import com.example.magnisetesttask.data.remote.retrofit.dto.DateRangeResponse
-import com.example.magnisetesttask.data.remote.retrofit.dto.ExchangesResponse
 import com.example.magnisetesttask.data.remote.retrofit.dto.InstrumentsResponse
-import com.example.magnisetesttask.data.remote.retrofit.dto.ProvidersResponse
-import com.example.magnisetesttask.data.remote.retrofit.dto.TokenResponse
-import retrofit2.http.Body
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.POST
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface FintachartsApi {
@@ -21,21 +11,6 @@ interface FintachartsApi {
     suspend fun getInstruments(
         @Query("provider") provider: String
     ): InstrumentsResponse
-
-    @GET("/api/instruments/v1/providers")
-    suspend fun getProviders(): ProvidersResponse
-
-    @GET("/api/instruments/v1/exchanges")
-    suspend fun getExchanges(): ExchangesResponse
-
-    @GET("/api/bars/v1/bars/count-back")
-    suspend fun getCountBack(
-        @Query("instrumentId") instrumentId: String,
-        @Query("provider") provider: String,
-        @Query("interval") interval: Int,
-        @Query("periodicity") periodicity: String,
-        @Query("barsCount") barsCount: Int
-    ): CountBackResponse
 
     @GET("/api/bars/v1/bars/date-range")
     suspend fun getDateRange(
--- app/src/main/java/com/example/magnisetesttask/data/remote/retrofit/dto/ProvidersResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.magnisetesttask.data.remote.retrofit.dto
-
-import com.google.gson.annotations.SerializedName
-
-data class ProvidersResponse(
-    @SerializedName("data") val dataList: List<String>
-)
